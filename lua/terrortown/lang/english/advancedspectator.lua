1a3bbf9702f13b4951f53d34015d0391223a2930
Tim Goll <tim@timgoll.de>
2020-09-18 18:34:19 +0200

    fixed language identifier

--- lua/terrortown/lang/english/advancedspectator.lua
+++ lua/terrortown/lang/english/advancedspectator.lua
@@ -1,4 +1,4 @@
-local L = LANG.GetLanguageTableReference("deutsch")
+local L = LANG.GetLanguageTableReference("english")
 
 L["ttt2_aspectator_wallhack"] = "Toggle admin wallhack"
 L["ttt2_aspectator_started_wallack"] = "Started admin only wallhack."
