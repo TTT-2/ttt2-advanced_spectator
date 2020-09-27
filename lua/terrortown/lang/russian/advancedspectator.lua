378caf645a76764747b3902c99b3432f0748ad2d
Blueberryy <igory.ygr200@gmail.com>
2020-09-27 21:08:03 +0500

    Create advancedspectator.lua

---
+++ lua/terrortown/lang/russian/advancedspectator.lua
@@ -0,0 +1,5 @@
+local L = LANG.GetLanguageTableReference("Русский")
+
+L["ttt2_aspectator_wallhack"] = "Переключить видение через стены(администратор)"
+L["ttt2_aspectator_started_wallack"] = "Включено видение через стены(для администраторов)."
+L["ttt2_aspectator_stopped_wallack"] = "Отключено видение через стены(для администраторов)."
