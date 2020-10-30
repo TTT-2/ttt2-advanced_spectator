528e8f8a5e7c529a6360680685cae1ff0cad469c
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-10-30 19:27:08 +0100

    Create advancedspectator.lua
    french translation : advanced spectator

---
+++ lua/terrortown/lang/french/advancedspectator.lua
@@ -0,0 +1,5 @@
+local L = LANG.GetLanguageTableReference("french")
+
+L["ttt2_aspectator_wallhack"] = "Activer/Désactiver le Wallhack ADMIN (pour les administrateurs seulement)"
+L["ttt2_aspectator_started_wallack"] = "Wallhack ADMIN Activé (pour les administrateurs seulement)."
+L["ttt2_aspectator_stopped_wallack"] = "Wallhack ADMIN Désactivé (pour les administrateurs seulement)."
