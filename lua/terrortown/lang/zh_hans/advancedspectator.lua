8b08fa2d54c08ef1401869e595c29007aa84ef9e
sbzl <92261479+sbzlzh@users.noreply.github.com>
2022-07-24 12:52:41 +0800

    Add simplified Chinese

---
+++ lua/terrortown/lang/zh_hans/advancedspectator.lua
@@ -0,0 +1,17 @@
+local L = LANG.GetLanguageTableReference("zh_hans")
+
+L["ttt2_aspectator_wallhack"] = "切换管理员透视"
+L["ttt2_aspectator_started_wallack"] = "开启管理员透视."
+L["ttt2_aspectator_stopped_wallack"] = "关闭管理员透视."
+
+L["submenu_server_addons_advancedspectator_title"] = "高级旁观者"
+L["header_addons_advancedspectator_hud"] = "高级旁观者HUD设置"
+L["header_addons_advancedspectator_wallhack"] = "高级旁观者透视设置"
+
+L["label_aspectator_display_role"] = "在HUD中显示角色让观众看见"
+L["label_aspectator_admin_only"] = "仅向管理员显示HUD元素"
+L["label_aspectator_enable_wallhack"] = "观看时启用透视"
+L["label_aspectator_display_wallhack_role"] = "为观众展示角色"
+L["label_aspectator_admin_wallhack"] = "启用管理员透视来调试角色"
+
+L["help_aspectator_admin_wallhack"] = "在此处启用此管理员透视功能不会自动启动.还需要在绑定菜单中绑定一个键.该键用于切换透视."
